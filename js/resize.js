90c6c1c2d94eba7390ed5978cb556d63156d7569
1098620475@qq.com <1098620475@qq.com>
2017-03-09 12:39:52 +0800

    new

--- js/resize.js
+++ js/resize.js
@@ -2,11 +2,20 @@ function resize(pm,type){
     var type=type||'x';
     var cW=document.documentElement.clientWidth||document.body.clientWidth;
     var cH=document.documentElement.clientHeight||document.body.clientHeight;
-
     if(type=='x'){
         var scale=cW/pm*100+'px';
     }else{
         var scale=cH/pm*100+'px';
     }
     document.getElementsByTagName('html')[0].style.fontSize=scale;
+    window.onresize=function(){
+        var cW=document.documentElement.clientWidth||document.body.clientWidth;
+        var cH=document.documentElement.clientHeight||document.body.clientHeight;
+        if(type=='x'){
+            var scale=cW/pm*100+'px';
+        }else{
+            var scale=cH/pm*100+'px';
+        }
+        document.getElementsByTagName('html')[0].style.fontSize=scale;
+    }
 }
