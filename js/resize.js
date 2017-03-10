f88000d96b94498feaba2955a743dc82f5daffbb
1098620475@qq.com <1098620475@qq.com>
2017-03-10 12:00:46 +0800

    zuihou

---
+++ js/resize.js
@@ -0,0 +1,24 @@
+function resize(pm,type){
+    var type=type||'x';
+    var cW=document.documentElement.clientWidth||document.body.clientWidth;
+    var cH=document.documentElement.clientHeight||document.body.clientHeight;
+    if(type=='x'){
+        var scale=cW/pm*100+'px';
+    }else{
+        var scale=cH/pm*100+'px';
+    }
+    document.getElementsByTagName('html')[0].style.fontSize=scale;
+    window.onresize=function(){
+        var cW=document.documentElement.clientWidth||document.body.clientWidth;
+        var cH=document.documentElement.clientHeight||document.body.clientHeight;
+        // if(cW<=1200){
+        //     cW=1200
+        // }
+        if(type=='x'){
+            var scale=cW/pm*100+'px';
+        }else{
+            var scale=cH/pm*100+'px';
+        }
+        document.getElementsByTagName('html')[0].style.fontSize=scale;
+    }
+}
