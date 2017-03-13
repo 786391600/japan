b0f04fcaf421796fe10d7ed485f141216bbd75a2
1098620475@qq.com <1098620475@qq.com>
2017-03-13 16:38:59 +0800

    new

--- js/skip.js
+++ js/skip.js
@@ -13,8 +13,13 @@ function skip(){
             var sC=($('.con-continer ul').eq(8).offset().top);
             $('body,html').animate({scrollTop:sC},300);
         }else if(cs=='three'){
-            var sC=($('.con-continer ul').eq(13).offset().top+150);
-            $('body,html').animate({scrollTop:sC},300);
+            if($(window).scrollTop()>=$('.header').get(0).offsetHeight){
+                var sC=($('.con-continer ul').eq(12).offset().top-$('.header').height()-20);
+                $('body,html').animate({scrollTop:sC},300);
+            }else{
+                var sC=($('.con-continer ul').eq(12).offset().top+270);
+                $('body,html').animate({scrollTop:sC},300);
+            }
         }
    $('.lj').eq(0).click(function(){
        if($(window).scrollTop()>=$('.header').get(0).offsetHeight){
